77405d98445b3d341c4246113f07c2a44f209f41
myongdol <myongdol132@gmail.com>
2023-06-15 17:55:15 +0900

    [feat}에듀케이션 컴포넌트 추가

---
+++ src/components/Education.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Education() {
+    return (
+        <article>
+            <h2>교육 관련 내용</h2>
+            <ul>
+                <li>2023.06.15 ~ 2023.06.15</li>
+                <li>인터넷강의</li>
+                <li>2023.06.15 ~ 2023.06.15</li>
+                <li>부트캠프 수강</li>
+            </ul>
+        </article>
+    )
+}
