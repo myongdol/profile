8f60b4d3a40bb2538257abc7cd12b26c6cf82b0b
myongdol <myongdol132@gmail.com>
2023-06-15 17:52:55 +0900

    [feat]커리어 컴포넌트 추가

---
+++ src/components/Career.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Career () {
+    return (
+        <article>
+            <h2>경력</h2>
+            <ul>
+                <li>경력을 쓰기전에</li>
+            </ul>
+            <h2>구조를 잡아보자</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Redux</li>
+                <li>Styled-Components, Emotion</li>
+            </ul>
+        </article>
+    )
+}
