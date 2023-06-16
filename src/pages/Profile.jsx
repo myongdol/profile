6bc5e91ecd31a3b1fcace00b47b50a9a54d430a0
myongdol <myongdol132@gmail.com>
2023-06-16 22:58:01 +0900

    [feat] 프로필에 테크스택 추가

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,12 +5,13 @@ import Info from "../components/Info";
 import Career from "../components/Career";
 import Education from "../components/Education";
 import Project from "../components/Project";
-
+import techStacks from "../components/techStacks";
 
 export default function Profile() {
     return (
         <main css={profileContainer}>
             <Info />
+            <techStacks />
             <Career />
             <Education />
             <Project />
