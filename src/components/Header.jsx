326579f2b7cc7264518e37a2a71670bfa213580a
myongdol <myongdol132@gmail.com>
2023-06-16 23:25:02 +0900

    [design] 헤더 컴포넌트 디자인 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,46 @@
 import React from "react";
 import { css } from "@emotion/css";
 import { NavLink } from "react-router-dom";
+import { contentFontSize16, mainColor } from "../style/main";
 
 
 export default function Header() {
     return (
         <nav css={headerContainer}>
+            <ul>
             <li>
                 <NavLink to='/'>HOME</NavLink>
             </li>
             <li>
                 <NavLink to='/profile'>Profile</NavLink>
             </li>
-
+            </ul>
         </nav>
     )
 }
 
 const headerContainer = css`
-    border: 1px solid;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
 
+    ul{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 100px;
+        ${contentFontSize16}
+    }
+    
     li{
         cursor: pointer;
-        &:hover,
-        .active{
+        &:hover {
+            background-color: ${mainColor};
+        }
+        &:active{
             border-bottom: 2px solid blue;
+            background-color: ${mainColor};
         }
     }
     a{
