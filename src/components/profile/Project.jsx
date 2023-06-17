05c718361e0e8c8f62c2335c0950ea8d69302b07
myongdol <myongdol132@gmail.com>
2023-06-17 23:17:14 +0900

    [design]프로젝트 컴포넌트에 스타일 변수 적용 및 css 디자인 일부변경

--- src/components/profile/Project.jsx
+++ src/components/profile/Project.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { URLs } from "../../constant";
+import { css } from "@emotion/css";
+import { mainColor, boxTextColor, contentFontSize16, contentFontSize30, grayTitleBorderBottom } from "../../style/main";
+
+
 
 export default function Project() {
     return (
-        <article>
+        <article css={projectContainer}>
             <h2>프로젝트 목록</h2>
             <section>
                 <img src='https://avatars.githubusercontent.com/u/110724985?v=4' alt="dd" />
@@ -49,3 +53,56 @@ export default function Project() {
         </article>
     )
 }
+
+const projectContainer = css`
+  margin: 30px 0;
+  h2 {
+    ${grayTitleBorderBottom};
+    ${contentFontSize30}
+  }
+  div {
+    margin: 20px 0;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 30px;
+    section {
+      border-radius: 10px;
+      border-width: 1.5px;
+      overflow: hidden;
+      ${contentFontSize16};
+      cursor: pointer;
+      box-shadow: rgb(15 15 15 / 10%) 2px 4px 4px 0px, rgb(15 15 15 / 10%) 4px 4px 10px;
+      transition: all 0.3s ease-in-out;
+      transform: translateY(0);
+      opacity: 0.7;
+      &:hover {
+        transition: all 0.3s ease-in-out;
+        transform: translateY(-8px);
+        opacity: 1;
+      }
+      img {
+        width: 100%;
+      }
+      p:first-of-type {
+        font-weight: bold;
+        background-color: ${mainColor};
+        display: inline-block;
+      }
+      p {
+        margin: 0 20px;
+      }
+    }
+  }
+  .tech-stack {
+    display: flex;
+    gap: 10px;
+    margin: 0 20px;
+    li {
+      padding: 3px 6px;
+      background-color: ${mainColor};
+      color: ${boxTextColor};
+      border-radius: 3px;
+    }
+  }
+  
+  `;
