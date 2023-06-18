bdf771a49ac3e4e98187b3d3b334bb0cec597018
myongdol <myongdol132@gmail.com>
2023-06-19 00:47:01 +0900

    [feat] useFetch 커스텀 훅 생성

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export function useFetch(url, storage) { 
+    const [dataList, setDataList] = useState([]);
+
+
+    const getData = async () => {
+        await axios.get(url).then((res) => {
+            const list = res.data[`${storage}`];
+            setDataList(list);
+        });
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+    
+    return {dataList}
+}
