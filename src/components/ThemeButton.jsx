6b3a0053e61732de879f7a2bc4db1961a2e06488
myongdol <myongdol132@gmail.com>
2023-06-20 23:57:50 +0900

    [feat] 테마 변경 하는 버튼 컴포넌트 추가

---
+++ src/components/ThemeButton.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { css } from "@emotion/react";
+import { useSelector } from "react-redux";
+
+
+
+
+export default function ThemeButton({ handleChangeMode }) {
+    const state = useSelector((state) => state);
+  
+    return (
+      <button css={modeButtonContainer} onClick={handleChangeMode}>
+        {state ? "🤍" : "🖤"}
+      </button>
+    );
+  }
+
+
+  const modeButtonContainer = css`
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+  background-color: unset;
+  border: none;
+  font-size: 6rem;
+  z-index: 999;
+  cursor: pointer;
+`;
