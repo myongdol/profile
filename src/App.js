1289f101e4076b24e83328ed6527137fa374e83e
myongdol <myongdol132@gmail.com>
2023-06-21 00:04:47 +0900

    [chore] state 관리 컴포넌트 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import Header from "./components/Header";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeThemeMode } from "./redux/actions/themeAction";
+import ThemeButton from "./components/ThemeButton";
+
+
 
 export default function App() {
-  return <Header />
+  const state = useSelector((state) => state);
+  const [theme, setTheme] = useState(state)
+
+  const dispatch = useDispatch();
+  
+  const handleChangeMode = () => {
+    dispatch(changeThemeMode(!theme));
+    setTheme(!theme);
+  };
+
+
+  return (
+    <>
+      <Header />
+      <ThemeButton handleChangeMode={handleChangeMode} />
+    </>
+    )
 }
 
