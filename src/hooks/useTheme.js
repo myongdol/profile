12a2d5d9b86d6129447b2f94afdc844a48ca11b4
myongdol <myongdol132@gmail.com>
2023-06-22 23:33:14 +0900

    [feat] useTheme hook 생성

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,16 @@
+import { useSelector, useDispatch } from "react-redux";
+import {changeTheme} from "../redux/actions/themeAction"
+
+
+export function useTheme() {
+    const theme = useSelector((state) => state); 
+    const dispatch = useDispatch();
+
+
+    const setTheme = (theme) => {
+        dispatch(changeTheme(theme));
+
+    };
+
+    return [theme, setTheme];
+};
