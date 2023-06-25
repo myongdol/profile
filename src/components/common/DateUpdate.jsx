6413370f4052632f5368b063488ace7ab90c9d21
myongdol <myongdol132@gmail.com>
2023-06-25 22:09:14 +0900

    [feat] 업데이트 날짜 알려주는 컴포넌트 추가

---
+++ src/components/common/DateUpdate.jsx
@@ -0,0 +1,11 @@
+export default function DateUpdate({yyyymmdd}) {
+    return (
+        <div className="date-update" style={{textAlign: "right"}}> 
+            업데이트 : {YYYY_MM_DD(yyyymmdd)}
+        </div>
+    )
+}
+
+function YYYY_MM_DD(yyyymmdd) {
+    return `${yyyymmdd.slice(0, 4)}.${yyyymmdd.slice(4, 6)}.${yyyymmdd.slice(6, 8)}`;
+}
