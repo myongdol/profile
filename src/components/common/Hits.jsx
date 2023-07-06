183a8bcbc28bb19f026b64a402f0af46e06d2ce0
myongdol <myongdol132@gmail.com>
2023-07-06 22:21:04 +0900

    [feat] Hits에 noHits 추가

--- src/components/common/Hits.jsx
+++ src/components/common/Hits.jsx
@@ -1,5 +1,15 @@
+import { useSearchParams } from "react-router-dom";
+
+
+
 export function Hits({url = window.location.href}) {
     const encodeUrl = encodeURIComponent(url);
+    const [searchParams] = useSearchParams();
+
+    if (searchParams.get("noHits") != null) {
+        return <div style={{height: "23px"}} />
+    }
+
 
     return ( 
         <a
@@ -7,6 +17,7 @@ export function Hits({url = window.location.href}) {
         style={{
             display: "block",
             textAlign: "right",
+            height: "23px",
             }}>
             <img 
                 src={`https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=${encodeUrl}&count_bg=%23000000&title_bg=%23777777&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false`}
