4c999d5ab44c89f5b2944c5a158153bc269125e6
myongdol <myongdol132@gmail.com>
2023-07-08 18:04:31 +0900

    [fix] default 빠짐으로 인한 lazy오류 해결

--- src/components/Resume/TechStack.jsx
+++ src/components/Resume/TechStack.jsx
@@ -6,7 +6,7 @@ import { useTheme } from "../../hooks/useTheme";
 import { parseBoldString } from "../../js/ParseBoldString";
 
 
-export function TechStack() {
+export default function TechStack() {
     const fetchUrl = "/data/techStackData.json";
     const fetchStorage = "techStackData";
     const {dataList: techStackData} = useFetch(fetchUrl, fetchStorage);
