dd02520ec2bbe5008bfdd1400963fd89113592a4
myongdol <myongdol132@gmail.com>
2023-07-08 18:33:03 +0900

    [chore] constant에 Work추가

--- src/constant.js
+++ src/constant.js
@@ -2,9 +2,6 @@ export const URLs = {
     email: "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=myongdol132@gmail.com",
     github: "https://github.com/myongdol",
     blog: "https://blog.naver.com/ares132",
-    project1 : "a",
-    project2 : "b",
-    project13: "c",
 }
 
 
@@ -16,6 +13,7 @@ export const ResumeType = {
         "Info",
         "Introduce",
         "Career",
+        "Work",
         "TechStack",
         "Education",
         "ProjectSection",
@@ -26,6 +24,7 @@ export const ResumeType = {
         "ProfileImage",
         "Name",
         "Career",
+        "Work",
         "TechStack",
         "Education",
         "ProjectSection",
