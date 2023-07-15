d1fde888f5c2f732157814c6f575a1834db43e32
myongdol <myongdol132@gmail.com>
2023-07-15 23:18:32 +0900

    [chore]업데이트 날짜변경

--- src/components/Resume/Info.jsx
+++ src/components/Resume/Info.jsx
@@ -23,7 +23,7 @@ export default function Info() {
         <article name='Info' css={infoContainer(theme)}>
             
             <Hits />
-            <DateUpdate yyyymmdd={"20230714"} />
+            <DateUpdate yyyymmdd={"20230715"} />
 
 
             <section>
