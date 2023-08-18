060ed3632159028615bdfd14310c4f12aa6a5054
myongdol <myongdol132@gmail.com>
2023-08-19 00:27:52 +0900

    [fix] vercel 배포 환경에서 경로이동 안되는 현상 발견, 수정시도

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -4,27 +4,34 @@ import {Resume} from "../pages/Resume";
 import {Home} from "../pages/Home";
 import { useEffect, Suspense } from "react";
 import { useLocation } from "react-router-dom";
-
-
+import { useNavigate } from 'react-router-dom';
 
 export function Router() {
-    const { pathname } = useLocation();
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-    return (
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/profile' element={<Navigate to='/resume' replace={true} />} />
-        <Route
-          path='/resume'
-          element={
-            <Suspense fallback={<>로딩 중</>}>
-              <Resume />
-            </Suspense>
-          }
-        />
-      </Routes>
-    );
-};
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route
+        path='/profile'
+        element={() => {
+          navigate('/resume', { replace: true });
+          return null;
+        }}
+      />
+      <Route
+        path='/resume'
+        element={
+          <Suspense fallback={<>로딩 중</>}>
+            <Resume />
+          </Suspense>
+        }
+      />
+    </Routes>
+  );
+}
